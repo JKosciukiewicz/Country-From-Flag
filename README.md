974a5c3a1585a8580ebf34149897e2e919dce8d8
JKosciukiewicz <jakub.kosciukiewicz@student.uj.edu.pl>
2024-05-24 23:21:21 +0200

    Everythig done

--- README.md
+++ README.md
@@ -5,33 +5,33 @@
 
 ## Installation
 <ol>
-<li>Clone the repository  
-</li> 
+<li>Clone the repository
+</li>
 <li>Create conda environment with <br><br>
 
-    
+
     conda env create -f environment.yml
-    
+
 <li>Activate the environment with<br><br>
-    
+
     conda activate country_from_flag
 </li>
 </ol>
 
 ## Usage
 <ol>
-<li> <b>Model needs to be trained first</b>, all the scripts required to train model and download dataset are located in the <b>run_training.py</b> file.  
+<li> <b>Model needs to be trained first</b>, all the scripts required to train model and download dataset are located in the <b>run_training.py</b> file.
 Use to execute the script. Dataset will be created automatically if necessary and model will train
 
     python run_training.py
 </li>
-<li>With model trained you can now use it to predict images.  
+<li>With model trained you can now use it to predict images.
 The file will run in a infinite loop, to guess the flag simply paste path of the file into command line. Run the following file for the inference pipeline.<br><br><b>The pipeline will always use the latest version of trained model !!!</b><br><br>
-    
+
     python guess_country.py
-</li>
-</ol>
+
 Alternatively the whole process can be done by running the following command
 
     bash run_app.sh
-
+</li>
+</ol>
--- prepare_dataset.py
+++ prepare_dataset.py
@@ -8,6 +8,12 @@ import csv
 
 
 def prepare_dataset():
+
+    def save_to_set(files, source_dir, destination_dir, split):
+        for file in files:
+            shutil.copy(os.path.join(source_dir, file), os.path.join(destination_dir, file))
+            csv_data.append([country, file, split])
+
     # Downloads flags and prepare dataset
     input_dir = './Data/Flags'
     train_dir = './Data/Train'
@@ -24,7 +30,7 @@ def prepare_dataset():
 
     # Augment images
     print("Augmenting images ...")
-    augment_and_save(target_count=50, input_dir='./Flags', output_dir='./Data/Flags', prefix='')
+    augment_and_save(target_count=50, input_dir='./Flags', output_dir='./Data/Flags')
 
     # Create 80/10/10 test/train/val split
     print("Creating train/test/val split ...")
@@ -36,48 +42,35 @@ def prepare_dataset():
     test_dir = './Data/Test'
 
     # Create output directories if they don't exist
-    # Create output directories if they don't exist
     os.makedirs(train_dir, exist_ok=True)
     os.makedirs(test_dir, exist_ok=True)
 
-    # List to hold CSV data
     csv_data = []
-    country_indices = {}
-    # Traverse each country directory
-    for idx, country in enumerate(os.listdir(input_dir)):
+    for country in os.listdir(input_dir):
         country_path = os.path.join(input_dir, country)
         if os.path.isdir(country_path):
-            # Create corresponding train/test country directories
+            # Create ttrain/test directories
             train_country_dir = os.path.join(train_dir, country)
             test_country_dir = os.path.join(test_dir, country)
             os.makedirs(train_country_dir, exist_ok=True)
             os.makedirs(test_country_dir, exist_ok=True)
 
-            # List all files in the country directory
             all_files = [f for f in os.listdir(country_path) if os.path.isfile(os.path.join(country_path, f))]
 
             # Split the files into train and test sets
             train_files, test_files = train_test_split(all_files, test_size=0.2, random_state=42)
 
-            country_indices[idx] = country
-
-            # Function to copy files to their respective directories and collect CSV data
-            def copy_files_and_collect_csv_data(files, source_dir, destination_dir, split):
-                for file in files:
-                    shutil.copy(os.path.join(source_dir, file), os.path.join(destination_dir, file))
-                    csv_data.append([country, file, split])
-
-            # Copy train and test files to their respective directories and collect CSV data
-            copy_files_and_collect_csv_data(train_files, country_path, train_country_dir, 'train')
-            copy_files_and_collect_csv_data(test_files, country_path, test_country_dir, 'test')
+            # Save files to train and test folders
+            save_to_set(train_files, country_path, train_country_dir, 'train')
+            save_to_set(test_files, country_path, test_country_dir, 'test')
 
 
-    # Write CSV data to file
-    csv_file_path = './Data/split_data.csv'
-    with open(csv_file_path, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['class', 'img', 'split'])  # Write header
-        csv_writer.writerows(csv_data)
+        # Write CSV data to file
+        csv_file_path = './Data/split_data.csv'
+        with open(csv_file_path, mode='w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['class', 'img', 'split'])  # Write header
+            csv_writer.writerows(csv_data)
 
     print("Dataset complete")
 
